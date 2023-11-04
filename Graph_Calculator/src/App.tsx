b9602f858b106adeb72e39d642184b78b6a25a0f
yuantianle <yuant.osu@gmail.com>
2023-11-04 02:55:27 -0700

    finished the side list for the calculator

--- Graph_Calculator/src/App.tsx
+++ Graph_Calculator/src/App.tsx
@@ -1,32 +1,77 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import GraphCanvas from './components/GraphCanvas.tsx'
-import InputBox from './components/InputBox.tsx'
+import GraphCanvas from './components/GraphCanvas';
+import InputBox from './components/InputBox';
 
 function App() {
+    // Predefined formulas list
+    const formulasList = [
+        { formula: 'z^2=cos(x)^2+sin(y)^2', defaultText: 'Mattries' },
+        { formula: 'cos(z)^2=cos(x)^2+sin(y)^2', defaultText: 'Time Machine' },
+        { formula: 'x^2+y^2+z^2=40', defaultText: 'Sphere' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: 'x^2+y^2-2*x=2*sqrt(x^2+y^2)', defaultText: 'Cartesian Heart' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        { formula: '(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y', defaultText: 'Default Text 2' },
+        // ... more formulas
+    ];
 
-  // graph canvas uses formula from input box
-  const [formula, setFormula] = useState('z=cos(x)^2+sin(y)^2')
-  const handleFormulaChange = (newFormula: string) => {
-      setFormula(newFormula);
-  }
-  //optional initial formula: 
-  //(0.64*sqrt(abs(x))-0.8+1.2^abs(x)*cos(200*x))*sqrt(cos(x))=y
-  
-  return (
-    <>
-    <h1>Marcus Graphing Calculator</h1>
-      <div className="App">
-        
-        <header className="App-header">
-          <InputBox onFormulaChange={handleFormulaChange} />
-          <div className="Canvas"><GraphCanvas formula={formula} /></div>
-        </header>
-      </div>
-    <h4>@Tianle Yuan</h4>
-    </>
-  )
-}//
+    // graph canvas uses formula from input box
+    const [formula, setFormula] = useState(formulasList[0]['formula']);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true); // State to control sidebar visibility
 
-export default App
+    const handleFormulaChange = (newFormula: string) => {
+        setFormula(newFormula);
+    };
+
+    // Function to handle formula selection from the list
+    const handleFormulaSelect = (formula: string) => {
+        setFormula(formula);
+    };
+
+    const toggleSidebar = () => { // Function to toggle sidebar
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    return (
+        <>
+            <h1>Marcus Graphing Calculator</h1>
+            <div className="App">
+                <header className="App-header">
+
+                    <aside className={`FormulaList ${isSidebarOpen ? 'open' : 'closed'}`}>
+                        <ul key={isSidebarOpen ? 'opened' : 'closed'}>
+                            {formulasList.map((item, index) => (
+                                <li key={index} onClick={() => handleFormulaSelect(item.formula)}
+                                style={{ animationDelay: `${index * 0.2}s` }} // Each item will start its animation 0.1s after the previous one
+                                >
+                                    <span className="default-text">{item.defaultText}</span>
+                                    <span className="formula-text">{item.formula}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </aside>
+                    <div className="Controls">
+                        <button onClick={toggleSidebar} className="ToggleSidebar">
+                            {isSidebarOpen ? 'Hide Examples' : 'Show Examples'}
+                        </button>
+                        <InputBox onFormulaChange={handleFormulaChange} value={formula} />
+                    </div>
+                    <div className="Canvas"><GraphCanvas formula={formula} /></div>
+                </header>
+            </div>
+            <h4>@Tianle Yuan</h4>
+        </>
+    );
+}
+
+export default App;
--- Graph_Calculator/src/components/InputBox.tsx
+++ Graph_Calculator/src/components/InputBox.tsx
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
 
 interface InputBoxProps {
+    value: string;
     onFormulaChange: (formula: string) => void;
 }
 
-const InputBox: React.FC<InputBoxProps> = ({ onFormulaChange }) => {
-    const [formula, setFormula] = useState("z=cos(x)^2+sin(y)^2");
+const InputBox: React.FC<InputBoxProps> = ({ onFormulaChange, value }) => {
+    //const [formula, setFormula] = useState("z=cos(x)^2+sin(y)^2");
 
     const handleFormulaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newFormula = event.target.value;
-        setFormula(newFormula);
-        onFormulaChange(newFormula);
+        onFormulaChange(event.target.value);
     };
 
     const clearFormula = () => {
-        setFormula("");
+        //setFormula("");
         onFormulaChange("");
     };
 
     return (
-        <div className="InputBox" style={{ marginLeft: "33%", marginBottom: '2%', marginTop: '-1%'}}>
+        <div className="InputBox" style={{ marginLeft: "8px"}}>
             <label htmlFor="formula-input">Formula: </label>
             <input
                 id="formula-input"
                 type="text"
-                value={formula}
+                value={value}
                 onChange={handleFormulaChange}
             />
             <button onClick={clearFormula} style={{
-        padding: '5px 10px', // Adjust padding to change the size
-        fontSize: '0.8em',  // Smaller font size for the button text
-        margin: '5px',      // Margin around the button
-        borderRadius: '4px', // Rounded corners (optional)
+                padding: '5px 10px', // Adjust padding to change the size
+                fontSize: '0.8em',  // Smaller font size for the button text
+                margin: '5px',      // Margin around the button
+                borderRadius: '8px', // Rounded corners (optional)
 
-    }}>Clear</button>
+            }}>Clear</button>
         </div>
     );
 };
